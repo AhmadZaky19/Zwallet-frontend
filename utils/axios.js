8557732f034b04d548e61885c89ea1584af75989
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-20 01:26:58 +0700

    update axios

--- utils/axios.js
+++ utils/axios.js
@@ -5,42 +5,44 @@ const axiosApiIntances = axios.create({
   baseURL: "https://zwalet.herokuapp.com",
 });
 
-if (Cookie.get("token")) {
-  // Add a request interceptor
-  axiosApiIntances.interceptors.request.use(
-    function (config) {
-      // Do something before request is sent
+// Add a request interceptor
+axiosApiIntances.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    if (Cookie.get("token")) {
       config.headers = {
         Authorization: `Bearer ${Cookie.get("token")}`,
       };
-      return config;
-    },
-    function (error) {
-      // Do something with request error
-      return Promise.reject(error);
     }
-  );
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 
-  // Add a response interceptor
-  axiosApiIntances.interceptors.response.use(
-    function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
-      return response;
-    },
-    function (error) {
-      // Any status codes that falls outside the range of 2xx cause this function to trigger
-      // Do something with response error
-      if (error.response.status === 403) {
-        // console.log(error.response);
+// Add a response interceptor
+axiosApiIntances.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 403) {
+      Cookie.remove("token");
+      Cookie.remove("id");
+
+      if (Cookie.get("token")) {
         alert(error.response.data.msg);
-        Cookie.remove("token");
-        Cookie.remove("id");
         window.location.href = "/login";
       }
-      return Promise.reject(error);
     }
-  );
-}
+    return Promise.reject(error);
+  }
+);
 
 export default axiosApiIntances;
